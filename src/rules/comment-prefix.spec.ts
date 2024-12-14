765450205b5414e49a3cd1e5757bd196727e672d
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2024-12-14 16:56:29 +0900

    test: refactor test code

--- src/rules/comment-prefix.spec.ts
+++ src/rules/comment-prefix.spec.ts
@@ -1,110 +1,55 @@
 import { describe, it, expect } from "vitest";
 import { RuleTester } from "eslint";
 import rule from "./comment-prefix";
+import { getInvalidMessage } from "./getInvalidMessage";
 
 describe("code-comments-rule", () => {
   const ruleTester = new RuleTester({
     languageOptions: { ecmaVersion: 2020, sourceType: "module" },
   });
   describe("デフォルトの設定では、", () => {
-    
-      it("TODO, FIXME 形式のコメントがエラーにならない", () => {
-        ruleTester.run("comment-prefix", rule, {
-          valid: [
-            {
-              code: "// TODO[TASK-123]: これを実装する",
-              filename: "test-file.js",
-            },
-            {
-              code: "// FIXME[BUG-456]: 修正する必要がある",
-              filename: "test-file.ts",
-            },
-          ],
-          invalid: [],
-        });
+    const validComments = [
+      "// TODO[TASK-123]: これを実装する",
+      "// FIXME[BUG-456]: 修正する必要がある",
+      "// NOTE: 実装に関する説明",
+      "// WIP: 未実装部分",
+      "// HACK: 後でリファクタリング",
+      "// eslint-disable",
+      "// ts-ignore",
+      "// @biome-disable-next-line",
+      "/** ブロックコメント */",
+      `/*
+        複数行コメントも許可
+      */`,
+    ];
+
+    const invalidComments = [
+      "// 間違ったコメント",
+      "// TODO: チケット番号が不足",
+    ];
+
+    it("有効なコメントがエラーにならない", () => {
+      ruleTester.run("comment-prefix", rule, {
+        valid: validComments.map((comment) => ({
+          code: comment,
+          filename: "test-file.js",
+        })),
+        invalid: [],
       });
-    
-      it("NOTE, WIP, HACK のコメントがエラーにならない", () => {
-        ruleTester.run("comment-prefix", rule, {
-          valid: [
-            {
-              code: "// NOTE: 実装に関する説明",
-              filename: "test-file.jsx",
-            },
-            {
-              code: "// WIP: 未実装部分",
-              filename: "test-file.tsx",
-            },
-            {
-              code: "// HACK: 後でリファクタリング",
-              filename: "test-file.mjs",
-            },
-          ],
-          invalid: [],
-        });
-      });
-    
-      it("無視されるコメントがエラーにならない", () => {
-        ruleTester.run("comment-prefix", rule, {
-          valid: [
-            {
-              code: "// eslint-disable",
-              filename: "test-file.js",
-            },
-            {
-              code: "// ts-ignore",
-              filename: "test-file.ts",
-            },
-            {
-              code: "// @biome-disable-next-line",
-              filename: "test-file.tsx",
-            },
-          ],
-          invalid: [],
-        });
-      });
-    
-      it("ブロックコメントがエラーにならない", () => {
-        ruleTester.run("comment-prefix", rule, {
-          valid: [
-            {
-              code: "/** ブロックコメント */",
-              filename: "test-file.js",
-            },
-            {
-              code: `/*
-                複数行コメントも許可
-              */`,
-              filename: "test-file.jsx",
-            },
-          ],
-          invalid: [],
-        });
-      });
-  
-    it("ルールにマッチしないコメントにはエラーが発生する", () => {
+    });
+
+    it("無効なコメントにはエラーが発生する", () => {
       ruleTester.run("comment-prefix", rule, {
         valid: [],
-        invalid: [
-          {
-            code: "// 間違ったコメント",
-            filename: "test.ts",
-            errors: [
-              {
-                message: 'Line comment "間違ったコメント" does not match any of the user-defined line rules.',
-              },
-            ],
-          },
-          {
-            code: "// TODO: チケット番号が不足",
-            filename: "test.ts",
-            errors: [
-              {
-                message: 'Line comment "TODO: チケット番号が不足" does not match any of the user-defined line rules.',
-              },
-            ],
-          },
-        ],
+        invalid: invalidComments.map((comment) => ({
+          code: comment,
+          filename: "test.ts",
+          errors: [
+            {
+              message: getInvalidMessage("Line", comment),
+            },
+          ],
+        })),
       });
     });
   });

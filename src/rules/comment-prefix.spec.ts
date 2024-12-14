dcf211b62db33d975dfd5cbbd5f6f7fac0bf3e43
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2024-12-14 16:44:24 +0900

    test: オプションを渡された時のテスト

--- src/rules/comment-prefix.spec.ts
+++ src/rules/comment-prefix.spec.ts
@@ -3,10 +3,10 @@ import { RuleTester } from "eslint";
 import rule from "./comment-prefix";
 
 describe("code-comments-rule", () => {
+  const ruleTester = new RuleTester({
+    languageOptions: { ecmaVersion: 2020, sourceType: "module" },
+  });
   describe("デフォルトの設定では、", () => {
-    const ruleTester = new RuleTester({
-      languageOptions: { ecmaVersion: 2020, sourceType: "module" },
-    });
     
       it("TODO, FIXME 形式のコメントがエラーにならない", () => {
         ruleTester.run("comment-prefix", rule, {
@@ -108,4 +108,144 @@ describe("code-comments-rule", () => {
       });
     });
   });
+
+  describe("ユーザーオプションによるカスタマイズされたルールのテスト", () => {
+    it("カスタム lineRules が正しく適用される", () => {
+      ruleTester.run("comment-prefix", rule, {
+        valid: [
+          {
+            code: "// CUSTOM[ABC-123]: カスタムルールにマッチ",
+            filename: "custom-file.js",
+            options: [
+              {
+                lineRules: ["^CUSTOM\\[[A-Z]{3}-\\d+\\]:"], // ユーザー定義のルール
+              },
+            ],
+          },
+        ],
+        invalid: [
+          {
+            code: "// INVALID: このコメントはカスタムルールにマッチしない",
+            filename: "custom-file.js",
+            options: [
+              {
+                lineRules: ["^CUSTOM\\[[A-Z]{3}-\\d+\\]:"], // ユーザー定義のルール
+              },
+            ],
+            errors: [
+              {
+                message: 'Line comment "INVALID: このコメントはカスタムルールにマッチしない" does not match any of the user-defined line rules.',
+                line: 1,
+                column: 1,
+              },
+            ],
+          },
+        ],
+      });
+    });
+  
+    it("カスタム blockRules が正しく適用される", () => {
+      ruleTester.run("comment-prefix", rule, {
+        valid: [
+          {
+            code: "/* BLOCK[456]: カスタムルールにマッチ */",
+            filename: "block-file.js",
+            options: [
+              {
+                blockRules: ["^BLOCK\\[\\d+\\]: .*"], // ユーザー定義のブロックルール
+              },
+            ],
+          },
+        ],
+        invalid: [
+          {
+            code: "/* 不正なブロックコメント */",
+            filename: "block-file.js",
+            options: [
+              {
+                blockRules: ["^BLOCK\\[\\d+\\]: .*"], // ユーザー定義のブロックルール
+              },
+            ],
+            errors: [
+              {
+                message: 'Block comment "不正なブロックコメント" does not match any of the user-defined block rules.',
+                line: 1,
+                column: 1,
+              },
+            ],
+          },
+        ],
+      });
+    });
+  
+    it("カスタム lineIgnoreRules が適用される", () => {
+      ruleTester.run("comment-prefix", rule, {
+        valid: [
+          {
+            code: "// SKIP: このコメントは無視される",
+            filename: "ignore-file.js",
+            options: [
+              {
+                lineRules: ["^TODO\\[[A-Z]{3}-\\d+\\]:"], // 有効なルール
+                lineIgnoreRules: ["^SKIP:"], // 無視するコメント
+              },
+            ],
+          },
+        ],
+        invalid: [
+          {
+            code: "// MISSING: このコメントはエラー",
+            filename: "ignore-file.js",
+            options: [
+              {
+                lineRules: ["^TODO\\[[A-Z]{3}-\\d+\\]:"], // 有効なルール
+                lineIgnoreRules: ["^SKIP:"], // 無視するコメント
+              },
+            ],
+            errors: [
+              {
+                message: 'Line comment "MISSING: このコメントはエラー" does not match any of the user-defined line rules.',
+                line: 1,
+                column: 1,
+              },
+            ],
+          },
+        ],
+      });
+    });
+  
+    it("ファイルパスが include/exclude ルールに従う", () => {
+      ruleTester.run("comment-prefix", rule, {
+        valid: [
+          {
+            code: "// TODO[PROJ-123]: このファイルはルール違反でもエラーにならない",
+            filename: "excluded-file.js",
+            options: [
+              {
+                exclude: ["**/excluded-*.js"],
+              },
+            ],
+          },
+        ],
+        invalid: [
+          {
+            code: "// TODO: このファイルはルールに従う",
+            filename: "included-file.js",
+            options: [
+              {
+                include: ["included-*.js"],
+              },
+            ],
+            errors: [
+              {
+                message: 'Line comment "TODO: このファイルはルールに従う" does not match any of the user-defined line rules.',
+                line: 1,
+                column: 1,
+              },
+            ],
+          },
+        ],
+      });
+    });
+  });  
 });

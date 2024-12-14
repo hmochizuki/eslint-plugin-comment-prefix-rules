483c4ea70970f34d3fe8458783d16bdcf28e9a33
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2024-12-14 11:52:53 +0900

    feat: examle.ts追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import customPlugin from "./dist/index.js";
 
 export default [
   {
+    files: ["example/**/*.ts", "example/**/*.js"],
     plugins: {
       custom: customPlugin,
     },
---
+++ example/example.ts
@@ -0,0 +1,7 @@
+// TODO[PROJ-123]: Implement the feature (✅ OK)
+// FIXME[BUG-456]: Fix the issue (✅ OK)
+// Just a comment (❌ エラー: Line ルールに一致しない)
+
+/*
+  This is a valid block comment. (✅ OK)
+*/
